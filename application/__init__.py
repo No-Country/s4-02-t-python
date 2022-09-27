bf18338f6d11859304358ba75fd54c52f776ff49
zeuspod <josseluism@gmail.com>
2022-09-27 13:21:45 -0400

    db connection add.

--- application/__init__.py
+++ application/__init__.py
@@ -1,12 +1,18 @@
 from flask import Flask
-#from flask_sqlalchemy import SQLAlchemy
+from pymongo import MongoClient
 
-app = Flask(__name__) 
+app = Flask(__name__)
 
-""" 
-db config
-app.config['SQLALCHEMY_DB_URI'] = ""
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Mongo URI
+MONGO_URI = 'mongodb+srv://hermes:jm9Sp2lBKEqHSV2i@hermes.awnisit.mongodb.net/?retryWrites=true&w=majority'
 
-db = SQLAlchemy(app) """
 
+def dbConnection():
+    try:
+        client = MongoClient(MONGO_URI)
+        db = client['users']
+        print('conexion success')
+    except ConnectionError:
+        print('Error de conexion')
+
+    return db
--- run.py
+++ run.py
@@ -1,9 +1,14 @@
 from application import app
 from config import config
+from application import dbConnection
+
+
+
+
+db = dbConnection()
 
 
 if __name__ == '__main__':
     app.config.from_object(config['development'])
     app.run()
-
 
