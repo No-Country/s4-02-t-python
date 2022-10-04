2acdb41eb1e1008926c5640e78516d302df449bf
sunflower <rasp17698@gmail.com>
2022-10-04 16:58:57 -0500

    fix: run.py & add Blueprint

--- run.py
+++ run.py
@@ -1,14 +1,10 @@
+
 from application import app
 from config import config
-from application import dbConnection
-
-
-
-
-db = dbConnection()
+from application.routes.users import user_bp
 
 
 if __name__ == '__main__':
+    app.register_blueprint(user_bp)
     app.config.from_object(config['development'])
     app.run()
-
