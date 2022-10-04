876af5f3a2d735d12333e6aa36097977b46a1ea6
sunflower <rasp17698@gmail.com>
2022-10-04 16:56:31 -0500

    fix: file '__init__.py'

--- application/__init__.py
+++ application/__init__.py
@@ -1,18 +1,7 @@
 from flask import Flask
-from pymongo import MongoClient
+
 
 app = Flask(__name__)
 
-# Mongo URI
-MONGO_URI = 'mongodb+srv://hermes:jm9Sp2lBKEqHSV2i@hermes.awnisit.mongodb.net/?retryWrites=true&w=majority'
 
 
-def dbConnection():
-    try:
-        client = MongoClient(MONGO_URI)
-        db = client['users']
-        print('conexion success')
-    except ConnectionError:
-        print('Error de conexion')
-
-    return db
