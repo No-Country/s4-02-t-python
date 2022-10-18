c82a249eabb557cd8b00b9b364834690710f1d8b
sunfl <armando187984@gmail.com>
2022-10-18 16:07:53 -0500

    add: route meetings

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from config import config
 from application.routes.users import user_bp
 from application.routes.post import post_bp
 from application.routes.medicines import medicines_bp
+from application.routes.meetings import meetings_bp
 
 
 
@@ -11,5 +12,6 @@ if __name__ == '__main__':
     app.register_blueprint(user_bp)
     app.register_blueprint(post_bp)
     app.register_blueprint(medicines_bp)
+    app.register_blueprint(meetings_bp)
     app.config.from_object(config['development'])
     app.run()
